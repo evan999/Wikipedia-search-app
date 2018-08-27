80d428e05d66f1fc6f09eafa8ffacf110d9e98c9
Evan Meshberg <emeshberge@gmail.com>
2018-08-27 03:01:35 +0000

    Made some modifications to API call components

--- viewer.js
+++ viewer.js
@@ -1,39 +1,29 @@
-window.onLoad(function() {
+$(document).ready(function() {
   /* when form is submitted */
+  // var resElement = document.getElementById('res');
   $('.form').submit(function(){
-    $('#res').html(" "); // set innerHtml of res div as blank
-    callWikipedia();
+    var resElement = document.getElementById('res');
+    resElement.innerHTML = " ";
+
+    searchQuery();
     return false;
   });
   /* when search button is clicked */
   $('#search-bttn').click(function(){
-    $('#res').html(" ");
-    callWikipedia();
+    var resElement = document.getElementById('res');
+    resElement.innerHTML = " ";
+    searchQuery();
   });
-  function callWikipedia(){
+  function searchQuery(){
     var q = $('#query').val();
     var apiCall = "https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch="+q+"&callback=?";
-    /*
-    fetch( apiCall, {
-    method: 'POST',
-    // Other init settings such as 'credentials'
-    } ).then( function ( response ) {
-    if ( response.ok ) {
-        return response.json();
-    }
-    throw new Error( 'Nothing was found. Please try again!' + response.statusText );
-    } ).then( function ( res ) {
-    // do something with data
-        var data = res.query.pages;
-        render(data);
-    });
-    */
+    
     $.ajax({
       url:  apiCall,
       dataType: 'jsonp',
       type: 'POST',
-      success: (res) =>{
-        var data = res.query.pages;
+      success: (response) =>{
+        var data = response.query.pages;
         render(data);
       },
       error: (error) => {
@@ -41,12 +31,14 @@ window.onLoad(function() {
       }
     });
     
+    
   }
   /* render function to append the search result pages */
-  function render(data){
-    var selectionLink ="http://en.wikipedia.org/?curid=";
+   function render(data){
+    var selection ="http://en.wikipedia.org/?curid=";
+    
     for(var url in data){
-      $('#res').append("<div id='resultdiv'><a target='_blank' href='"+selectionLink+data[url].pageid+"'><h3>"+data[url].title+"</h3><p>"+data[url].extract+"</p></a></div>");
+      $('#res').append("<div id='resultdiv'><a target='_blank' href='"+selection+data[url].pageid+"'><h3>"+data[url].title+"</h3><p>"+data[url].extract+"</p></a></div>");
     }
   }
 });
